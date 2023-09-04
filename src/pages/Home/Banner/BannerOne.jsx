f7f6b04f63b8a1e5d1d8e74c6324c11e2227c120
rabby6530 <you@example.com>
2023-09-04 19:07:38 +0600

    added slider text

--- src/pages/Home/Banner/BannerOne.jsx
+++ src/pages/Home/Banner/BannerOne.jsx
@@ -9,19 +9,39 @@ const BannerOne = () => {
             <div className="carousel w-full">
                 <div id="item1" className="carousel-item w-full">
                     <img src={banner4} className="w-full" />
-                    <div className="carousel-caption tex-black">
-                        <h5 >Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
+                    <div className=" lg:mb-24 mb-24 text-white absolute top-16 bottom-1/2 md:top-1/4 xl:bottom-1 lg:top-20 xl:top-1/4 lg:bottom-1/2 left-0 md:left-10 lg:left-20 xl:left-1/4  p-2  xl:p-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className="lg:my-20 xl:my-20 mb-20 ">
+                            <h1 className="text-2xl lg:text-4xl xl:text-4xl uppercase font-bold my-2 lg:my-3 xl:my-3">Well Come to our Chef and Recipe</h1>
+                            <p className="xl:text-lg">Who is lover Food for this chef and recipe . Everyone Welcome for delicious food item</p>
+                        </div>
                     </div>
                 </div>
                 <div id="item2" className="carousel-item w-full">
                     <img src={banner1} className="w-full" />
+                    <div className=" lg:mb-24 mb-24 text-white absolute top-16 bottom-1/2 md:top-1/4 xl:bottom-1 lg:top-20 xl:top-1/4 lg:bottom-1/2 left-0 md:left-10 lg:left-20 xl:left-1/4  p-2  xl:p-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className="lg:my-20 xl:my-20 mb-20 ">
+                            <h1 className="text-2xl lg:text-4xl xl:text-4xl uppercase font-bold my-2 lg:my-3 xl:my-3">Well Come to our Chef and Recipe</h1>
+                            <p className="xl:text-lg">Who is lover Food for this chef and recipe . Everyone Welcome for delicious food item</p>
+                        </div>
+                    </div>
                 </div>
                 <div id="item3" className="carousel-item w-full">
                     <img src={banner2} className="w-full" />
+                    <div className=" lg:mb-24 mb-24 text-white absolute top-16 bottom-1/2 md:top-1/4 xl:bottom-1 lg:top-20 xl:top-1/4 lg:bottom-1/2 left-0 md:left-10 lg:left-20 xl:left-1/4  p-2  xl:p-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className="lg:my-20 xl:my-20 mb-20 ">
+                            <h1 className="text-2xl lg:text-4xl xl:text-4xl uppercase font-bold my-2 lg:my-3 xl:my-3">Well Come to our Chef and Recipe</h1>
+                            <p className="xl:text-lg">Who is lover Food for this chef and recipe . Everyone Welcome for delicious food item</p>
+                        </div>
+                    </div>
                 </div>
                 <div id="item4" className="carousel-item w-full">
                     <img src={banner3} className="w-full" />
+                    <div className=" lg:mb-24 mb-24 text-white absolute top-16 bottom-1/2 md:top-1/4 xl:bottom-1 lg:top-20 xl:top-1/4 lg:bottom-1/2 left-0 md:left-10 lg:left-20 xl:left-1/4  p-2  xl:p-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className="lg:my-20 xl:my-20 mb-20 ">
+                            <h1 className="text-2xl lg:text-4xl xl:text-4xl uppercase font-bold my-2 lg:my-3 xl:my-3">Well Come to our Chef and Recipe</h1>
+                            <p className="xl:text-lg">Who is lover Food for this chef and recipe . Everyone Welcome for delicious food item</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="flex justify-center w-full py-2 gap-2">
